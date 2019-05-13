47c786b3d7c5858e61c56c82ff8a511e3c94c3d5
miiibdboy <50560477+miiibdboy@users.noreply.github.com>
2019-05-13 18:12:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("")
+let channel =     client.channels.get("577525798995755027")
 setInterval(function() {
 channel.send(`YA 5OYA YA 5OYA RAS ITIR ANA MACHI BNADAM`);
 }, 30)
